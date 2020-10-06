91b79d49616f36c90b855c10a39b2b18f73f9ad2
Thyago Vinicius Camilo Cunha <thyago.vccunha@gmail.com>
2020-10-05 22:56:05 -0300

    chore: changed the kotlinx.serialization to Jackson

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ val koin_version = "2.1.6"
 
 plugins {
     kotlin("jvm") version "1.4.10"
-    kotlin("plugin.serialization") version "1.4.10"
 }
 group = "dev.loosername"
 version = "1.0-SNAPSHOT"
@@ -17,27 +16,14 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib", KotlinCompilerVersion.VERSION))
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.0-RC2")
     implementation("io.javalin:javalin:3.11.0")
     implementation("org.slf4j:slf4j-simple:1.7.30")
     implementation("org.koin:koin-core:$koin_version")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.10.3")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.3")
     testImplementation(kotlin("test-junit"))
     testImplementation("org.koin:koin-test:$koin_version")
     testImplementation("io.mockk:mockk:1.10.2")
-}
-
-buildscript {
-    repositories {
-        jcenter()
-        mavenCentral()
-    }
-
-    dependencies {
-        val kotlinVersion = "1.4.10"
-
-        classpath(kotlin("gradle-plugin", version = kotlinVersion))
-        classpath(kotlin("serialization", version = kotlinVersion))
-    }
 }
 
 tasks.withType<KotlinCompile> {

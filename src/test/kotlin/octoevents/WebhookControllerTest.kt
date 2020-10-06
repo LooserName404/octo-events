c4ea2aa6c1dab137d6591a09faab524f17534685
Thyago Vinicius Camilo Cunha <thyago.vccunha@gmail.com>
2020-10-06 10:59:18 -0300

    test: ensure WebhookController pass the right data when calling create from WebhookService

--- src/test/kotlin/octoevents/WebhookControllerTest.kt
+++ src/test/kotlin/octoevents/WebhookControllerTest.kt
@@ -4,7 +4,12 @@ import io.javalin.http.BadRequestResponse
 import io.javalin.http.Context
 import io.mockk.*
 import octoevents.controllers.WebhookController
+import octoevents.models.entities.Webhook
 import octoevents.models.services.WebhookService
+import octoevents.models.unparsed.Organization
+import octoevents.models.unparsed.Repository
+import octoevents.models.unparsed.Sender
+import octoevents.models.unparsed.UnparsedWebhook
 import org.junit.Rule
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
@@ -13,6 +18,7 @@ import org.koin.test.KoinTestRule
 import org.koin.test.get
 import org.koin.test.mock.MockProviderRule
 import org.koin.test.mock.declareMock
+import java.util.*
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 
@@ -21,9 +27,7 @@ class WebhookControllerTest : KoinTest {
     private val ctx = mockk<Context>(relaxed = true)
 
     private val webhookServiceStub by lazy {
-        declareMock<WebhookService> {
-            every { create() } answers { ctx.status(201) }
-        }
+        spyk(WebhookService())
     }
 
     @get:Rule
@@ -46,10 +50,30 @@ class WebhookControllerTest : KoinTest {
     @Test
     fun `Should call create from WebhookService when its create is called`() {
         val sut = WebhookController()
-
+        val date = Date()
+        every { ctx.body<UnparsedWebhook>() } answers { UnparsedWebhook(createdAt = date) }
         sut.create(ctx)
+        verify { webhookServiceStub.create(Webhook(createdAt = date)) }
+    }
 
-        verify { webhookServiceStub.create() }
+    @Test
+    fun `Should pass the right data to WebhookService create method`() {
+        val event = "TestEvent"
+        val action = "TestAction"
+        val sender = Sender("TestSender")
+        val repository = Repository("TestRepo")
+        val organization = Organization("TestOrg")
+        val createdAt = Date(0)
+        val unparsedWebhook = UnparsedWebhook(event, action, sender, repository, organization, createdAt)
+
+        every { ctx.body<UnparsedWebhook>() } answers { unparsedWebhook }
+        val sut = WebhookController()
+        sut.create(ctx)
+        verify {
+            webhookServiceStub.create(
+                Webhook(event, action, sender.name, repository.full_name, organization.login, createdAt)
+            )
+        }
     }
 
 }

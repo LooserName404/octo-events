79345a88b7507d307290bd0274e42382044b3ad2
Thyago Vinicius Camilo Cunha <thyago.vccunha@gmail.com>
2020-10-07 17:42:58 -0300

    refactor: add support to database definition through properties file

--- src/main/kotlin/octoevents/config/DatabaseConfig.kt
+++ src/main/kotlin/octoevents/config/DatabaseConfig.kt
@@ -4,13 +4,19 @@ import octoevents.models.entities.WebhookTable
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.io.FileInputStream
+import java.util.*
 
-fun getPgConnection(): Database {
+fun getDbConnection(): Database {
+    val propertiesFile = FileInputStream("application.properties")
+    val prop = Properties()
+    prop.load(propertiesFile)
+
     val db = Database.connect(
-        url = "jdbc:postgresql://localhost:5432/octo",
-        driver = "org.postgresql.Driver",
-        user = "octo",
-        password = "octo"
+        url = prop["db_url"] as String,
+        driver = prop["db_driver"] as String,
+        user = prop["db_user"] as String,
+        password = prop["db_password"] as String
     )
     startSchemas(db)
     return db
--- src/main/kotlin/octoevents/config/KoinConfig.kt
+++ src/main/kotlin/octoevents/config/KoinConfig.kt
@@ -6,7 +6,7 @@ import octoevents.models.services.WebhookService
 import org.koin.dsl.module
 
 val injectableModules = module {
-    single { getPgConnection() }
+    single { getDbConnection() }
     single<WebhookRepository> { DbWebhookRepository() }
     single { WebhookService() }
 }
